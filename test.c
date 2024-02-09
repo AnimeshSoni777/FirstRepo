76191c78935818e32381f4a6ed6622443d169bdc
Animesh <animeshsoni777@gmail.com>
2024-02-09 19:20:41 +0530

    nothing

---
+++ test.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+/* int main() {
+   
+    int number;
+    printf("enter any number- ");
+    scanf("%d", &number);
+    printf("Number entered= %d", number);
+  
+     return 0;
+ } */
+
+#2
+/*int main(){
+    
+    double number;
+    char alphabet;
+    
+    printf("enter a num- ");
+    scanf("%lf", &number);
+    
+    printf("enter a letter- ");
+    scanf("\n%c", &alphabet);    
+
+    printf("\nentered number= %lf", number);
+    printf("\nentered alphabet= %c", alphabet);
+
+
+}*/
+
+#3
+/* int main() {
+    float x;
+    printf("Enter time in (24hrs): ");
+    scanf("%f",&x);
+    if (4.00<=x<=11.59){
+        printf("GOOD MORNING");
+    }
+    else if(12.00<=x<=16.59){
+        printf("GOOD AFTERNOON");
+    }
+    else if(17.00<=x<=19.59){
+        printf("GOOD EVENING");
+    }
+    else{
+        printf("GOOD NIGHT");
+    }
+
+    
+    return 0;
+}*/
+
+//                 #4 enetr prize and quantity if quantity is grater than 1000 give 10% discount on amount.
+/* int main() {
+    
+    int a,b,c,d,e,f,g,h;
+    printf("enter amount: ");
+    scanf("%d",&a);
+    printf("Quantity: ");
+    scanf("%d",&b);
+    printf("enter amount: ");
+    scanf("%d",&c);
+    printf("Quantity: ");
+    scanf("%d",&d);
+    printf("enter amount: ");
+    scanf("%d",&e);
+    printf("Quantity: ");
+    scanf("%d",&f);
+    printf("enter amount: ");
+    scanf("%d",&g);
+    printf("Quantity: ");
+    scanf("%d",&h);
+
+int x,y,z,u;
+    y=b+d+f+h;
+    x=a+c+e+g;
+    z=x*0.1;
+    u=x-z;
+    if(y<1000){
+    printf("TOTAL AMOUNT: %d",x);
+    }
+    else{printf("TOTAL AMOUNT(10percent discounted): %d",u);
+    }
+    
+    return 0;
+}*/
